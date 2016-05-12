c2086d35b65facec363fb02a5cd47aea5d8e1510
Jeremi Frazier <jerm_803@yahoo.com>
2016-05-12 13:44:33 -0400

    Update CasesarTwo.java
    add GUI later

--- CasesarTwo.java
+++ CasesarTwo.java
@@ -1,8 +1,23 @@
-
+/**
+ * This class is used to create encrytped and decryted message in the form of
+ * a Caesar Chiper determined by a shift.
+ * @author Jeremi Frazier
+ * @version 1.1
+ **/
+ 
 public class CaesarTwo {
+  
   private static int theShift = 0;
   public static char original;
-  private static int determineLength(String s){
+  
+  /**
+   * This aux or helper method determines the length of a string and is used to determine
+   * how many parts the message should be broken into.
+   * @param s The string to determine.
+   * @return int the length of the string s.
+   **/
+   
+   private static int determineLength(String s){
       if(s.length()%5 == 0){
         return s.length()/5;
       }
@@ -10,6 +25,14 @@ public class CaesarTwo {
         return (s.length()/5) +1;
       }
     }
+    
+    /**
+     * This method is called to break the casear cipher message
+     * into 4-5 parts and encrypt it based on the shift.
+     * @param s the original unecrpytped message.
+     * @param shift the amount to shift each character by.
+     * @return List<String> the encrypted message parts.
+     **/
 
     public static List<String> encodeStr(String s, int shift) {
     original = s.charAt(0);
@@ -30,7 +53,11 @@ public class CaesarTwo {
       }
       return myCodes;
     }
-
+    /** This method is shifts the string character by character and is hidden...
+     *  @param theString -message @param shift -amount to shift by.
+     *  @return String -encoded string.
+     **/
+     
     private static String shiftString(String theString, int shift) {
       StringBuilder strb = new StringBuilder();
       for (int i = 0; i < theString.length(); i++) {
@@ -53,10 +80,22 @@ public class CaesarTwo {
       }
       return strb.toString();
     }
-
+    /**
+     * This is a private accessor method. It is used to hold the shift value
+     * obtained in encrypt for usage in decrypt
+     * @return int -the shift amount
+     **/
+    
     private static int getShift() {
       return theShift;
     }
+    
+    /**
+     * This method is used to decode an encoded message
+     * The decoded message is equal to the original messgae.
+     * @param s -The encoded message in parts.
+     * @return Strin - the original message
+     **/
 
     public static String decode(List<String> s) {
       int shift = getShift();
